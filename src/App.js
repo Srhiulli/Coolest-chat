cf3693d6fd16324026e3ec94dd5c4af9ccdcc53d
Sarah Iulli <srhiulli@gmail.com>
2024-06-22 17:54:59 -0300

    homePage Init

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './pages/loginPage';
 import SignupPage from './pages/signupPage';
+import HomePage from './pages/homePage';
+// import PrivateRoute from './components/PrivateRoute';
 // import ChatPage from './pages/ChatPage';
 // import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   return (
     <Router>
-            <Link to="/">Login</Link>
+            <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
+            <Link to="/">Home</Link>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        {/* <Route path="/chat" element={<ChatPage />} /> */}
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        {/* <Route path="/chat" element={<PrivateRoute component={Chat} />} /> */}
       </Routes>
     </Router>
   );
